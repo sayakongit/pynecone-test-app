8e82f61dbcd9af04058dd15762e41a96347b3fd4
Sayak Saha <83216382+sayakongit@users.noreply.github.com>
2023-03-26 23:49:37 +0530

    removed extra codes

--- first_app/first_app.py
+++ first_app/first_app.py
@@ -1,9 +1,7 @@
 """Welcome to Pynecone! This file outlines the steps to create a basic app."""
 from pcconfig import config
-
 import pynecone as pc
 
-docs_url = "https://pynecone.io/docs/getting-started/introduction"
 filename = f"{config.app_name}/{config.app_name}.py"
 
 
